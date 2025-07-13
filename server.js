948d1ec5f884726eed67674bba326df0e83c3619
gusworkout <71235496+gusworkout@users.noreply.github.com>
2025-07-13 11:58:17 -0500

    Update server.js

--- server.js
+++ server.js
@@ -8,21 +8,8 @@ const PORT = 3000;
 
 app.use(bodyParser.json());
 app.use(express.static('public'));
-/*
-app.post('/guardar-json', (req, res) => {
-    const nuevoDato = req.body;
-    fs.readFile('./data/datos.json', 'utf8', (err, data) => {
-        const datos = data ? JSON.parse(data) : [];
-        datos.push(nuevoDato);
-        fs.writeFile('./data/datos.json', JSON.stringify(datos, null, 2), err => {
-            if (err) return res.status(500).send('Error al guardar datos.');
-            res.send('Datos guardados en JSON correctamente.');
-        });
-    });
-});
-*/
 
-app.post('/guardar-bd', (req, res) => {
+app.post('/guardarUsuario-bd', (req, res) => {
     const { id, correo, nombre, contrasena, telefono, direccion, fecha } = req.body;
     const conexion = mysql.createConnection({
         host: 'localhost',
@@ -32,6 +19,44 @@ app.post('/guardar-bd', (req, res) => {
     });
     const query = 'INSERT INTO usuario (id_usuario, correo, usuario, contrasena, telefono, direccion, fecha_cuenta) VALUES (?, ?, ?, ?, ?, ?, ?)';
     conexion.query(query, [id, correo, nombre, contrasena, telefono, direccion, fecha], (err, resultados) => {
+        if (err) {
+            console.error(err);
+            return res.status(500).send('Error al guardar en la base de datos.');
+        }
+        res.send('Datos guardados en MySQL correctamente.');
+    });
+});
+
+app.post('/guardarPubliacion-bd', (req, res) => {
+    const { fechapublicacion, idusuario, descripcion, etiqueta, 
+        idcomentario, numcomentario, idcompartido, numreaccion, vista, img} = req.body;
+    const conexion = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'redsocial1'
+    });
+    const query = 'INSERT INTO publicacion (fechapublciacion, idusuario, descripcion, etiqueta, idcomentario,numreacciones, idreaccion, vistas, imagen) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)';
+    conexion.query(query, [fechapublicacion, idusuario, descripcion, etiqueta, 
+        idcomentario, numcomentario, idcompartido, numreaccion, vista, img], (err, resultados) => {
+        if (err) {
+            console.error(err);
+            return res.status(500).send('Error al guardar en la base de datos.');
+        }
+        res.send('Datos guardados en MySQL correctamente.');
+    });
+});
+
+app.post('/guardarComentario-bd', (req, res) => {
+    const { id_comentario, fechacomentario, descripcion, respuesta, reaccion, numRespuestas} = req.body;
+    const conexion = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'redsocial1'
+    });
+    const query = 'INSERT INTO comentario (iddecomentario, fechadecomentario, descripcion, respuesta, reaccion, numeroderespuesta) VALUES (?, ?, ?, ?, ?, ?)';
+    conexion.query(query, [id_comentario, fechacomentario, descripcion, respuesta, reaccion, numRespuestas], (err, resultados) => {
         if (err) {
             console.error(err);
             return res.status(500).send('Error al guardar en la base de datos.');
